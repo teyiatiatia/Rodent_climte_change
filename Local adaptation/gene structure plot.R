610800b8601dcab4f3c9eab18fc7027a94c4f671
teyiatiatia <1475075804@qq.com>
2025-05-14 14:36:34 +0800

    Update gene structure plot.R

--- Local adaptation/gene structure plot.R
+++ Local adaptation/gene structure plot.R
@@ -1,62 +1,71 @@
-#1. vcf to pop freq
-
-
-
-
-
-
-
-setwd("D:/R/workdir/workspace/GEA/GEA_GPA_converg/allele_freq/PR/")
+# Allele frequency mapping of climate-related SNPs
+#1. vcf file
+setwd("D:/Ro/GEA/allele_freq/")
 .libPaths("D:/R/R-4.3.1/library")
 library(vcfR)
 library(adegenet)
 library(hierfstat)
+library(dplyr)
+library(xlsx)
+library(ggplot2)
+library(ggpubr)
 
-clim.points <-  read.table("D:/R/workdir/workspace/GEA/GF/clim.points",sep="\t",header=T)
-pr.env<-clim.points
-pr.vcf <- read.vcfR("D:/R/workdir/workspace/GEA/GEA_GPA_converg/allele_freq/PR/gene.recode.vcf")
-##bio8_12 foot_tail
-pr.vcf <- read.vcfR("D:/R/workdir/workspace/GEA/LFMM/env_K/SNP_anno/SNP_site/bio81215/lf_rd_bio8_2.recode.vcf")
-
+clim.points <- read.table("./input_file/clim.points",sep="\t",header=T)
+pr.env <- clim.points
+pr.vcf <- read.vcfR("./input_file/lfmm_rda.recode.vcf")
 
 df <-vcfR2genind(pr.vcf) #vcf2genind
 geno <- df$tab
 geno <- geno[,seq(1,ncol(geno)-1,2)]
-all(colnames(pr.vcf@gt)[-1] == pr.env$sample) #检查文件名
+all(colnames(pr.vcf@gt)[-1] == pr.env$sample) 
 pop(df) <- pr.env$pop #set populationp
-df$pop
 df$other <-pr.env
 pop(df) <- pr.env$pop #set populationp
 df.genpop <- adegenet::genind2genpop(df)
-library(dplyr)
-df.genpop$other <- pr.env[,c("pop","longitude", "latitude", "bio1", "bio2", "bio3", "bio4", "bio5", "bio6", "bio7", "bio8", "bio9", "bio10", "bio11", "bio12", "bio13", "bio14", "bio15", "bio16", "bio17", "bio18", "bio19")] |> distinct()
+df.genpop$other <- pr.env[,c("pop","longitude", "latitude")] |> distinct()
 all(rownames(df.genpop$tab) == df.genpop$other$pop)
 Xcount <- adegenet::tab(df.genpop, NA.method="zero")
-freq_mean <- adegenet::makefreq(df.genpop, missing="mean")  #函数“makefreq”从“genpop”对象中提取具有等位基因频率的表
+freq_mean <- adegenet::makefreq(df.genpop, missing="mean")  # Extract the allele frequency table
 library(stringr)
-freq_Q <- freq_mean[,(!is.na(str_extract(colnames(freq_mean),".0$")))] # 提取Q基因
+freq_Q <- freq_mean[,(!is.na(str_extract(colnames(freq_mean),".0$")))] 
 freq_pr <- freq_Q
 
-write.table(freq_pr, file = "D:/R/workdir/workspace/GEA/GEA_GPA_converg/allele_freq/PR/PR_pop_freq.txt", quote=F, sep = "\t")
+write.table(freq_pr, file = "./output_file/PR_pop_freq.txt", quote=F, sep = "\t")
 
+# Allele frequency table needs to have additional climate variables and population names included.
+# Allele frequency plot for Chr15_55598786
+data_1<-read.xlsx2("./output_file/PR_pop_freq.xlsx",
+                   sheetName = "Sheet1",
+                   header = TRUE,
+                   as.data.frame = TRUE,
+                   row.names = 1)
+data_1$bio8<-as.numeric(data_1$bio8)
+data_1$Chr15_55598786.1<-as.numeric(data_1$Chr15_55598786.1) 
+ggplot(data_1,aes(x=bio8,y=Chr15_55598786.1))+
+  ylab("Allele Frequency")+
+  geom_point(size=5,alpha=0.7,color="black")+
+  geom_smooth(method = "lm", formula = y~x, color = "black", fill = "gray")+
+  stat_cor(method = "pearson", label.x = 16, label.y = 1.2) +
+  theme_bw()+
+  theme(
+    panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
+    axis.text=element_text(size=18,color="black"),
+    legend.text=element_text(size=18),
+    axis.title.x=element_text(vjust=2, size=18),
+    axis.title.y =element_text(vjust=2, size=18)
+  )+geom_point(data = data_1, aes(x = bio8, y =Chr15_55598786.1,color=pop.1),size=5)+theme(legend.position = 'none')
+ggsave("./output_file/bio8_Chr15_55598786.1_SREBF1.pdf",width = 5, height = 5,units = "in") 
 
-
-####ggplot for gene structure
-
+#ggplot for gene structure of gene SREBF1
 .libPaths("D:/R/R-4.3.1/library")
-setwd("D:/R/workdir/workspace/GEA/LFMM/lfmm_prda_venn/output_set/positions/GEA_GPA_genes/")
-
+setwd("D:/Ro/GEA/allele_freq/gene_struc/")
 library(ggh4x)
 library(ggplot2)
 library(ggbio)
 library(GenomicRanges)
 
-
-#######################################
-setwd("D:/R/workdir/workspace/GEA/GEA_GPA_converg/gene_structure_map/PR/")
-####SREBF1_Chr15_55598786
-df<-read.table("./input_file/SREBF1_plot.txt",header=F)
-df_snp <- read.table("./input_file/SREBF1_plot_snp.txt",header = T)
+df<-read.table("./input_file/SREBF1_plot.txt",header=F) # This gene annotation information
+df_snp <- read.table("./input_file/SREBF1_plot_snp.txt",header = T) # The annotation information of this SNP
 
 SREBF1<-GRanges("Chr15",IRanges(df$V4,end=df$V5,group=df$V3))
 SREBF1_snp<-data.frame(xmin=unique(df_snp$x_location),
